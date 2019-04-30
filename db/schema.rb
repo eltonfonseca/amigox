f59035b591c66d7172f847da63f081e8dc84ca14
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2019-04-30 19:48:47 -0300

    #6 - Cria Grupos
    Criação de Grupos de Usuários para troca de mensagens e sorteio.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_203515) do
+ActiveRecord::Schema.define(version: 2019_04_30_224240) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
