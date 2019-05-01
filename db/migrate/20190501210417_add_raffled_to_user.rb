11f7550e9ac05c41834bbc486c2d87f37845affc
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2019-05-01 19:42:31 -0300

    Corrigindo Banco

--- db/migrate/20190501210417_add_raffled_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddRaffledToUser < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :users, :raffled, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_210417) do
+ActiveRecord::Schema.define(version: 2019_05_01_210325) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -47,9 +47,7 @@ ActiveRecord::Schema.define(version: 2019_05_01_210417) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.integer "raffled_id"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["raffled_id"], name: "index_users_on_raffled_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_id"], name: "index_users_on_user_id"
   end
